068afe157de3bdb97e92c8319d9fa57d7237cff8
tesla7 <77625961@qq.com>
2019-10-14 18:03:27 +0800

    development

--- app.py
+++ app.py
@@ -14,12 +14,17 @@ from flask import Flask, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES, \
     patch_request_class
 
+import subprocess
+
 app = Flask(__name__)
 app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()  # 文件储存地址
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)  # 文件大小限制，默认为16MB
+
+cwd = "/home/bayesai/yinhaozheng/Face-Liveness_detection/"
+shell = "./dlib_test_process"
 
 html = '''
     <!DOCTYPE html>
@@ -38,12 +43,17 @@ html = '''
 def upload_file():
     if request.method == 'POST':
         ir = photos.save(request.files['ir'])
-        raw = photos.save(request.files['raw'])
-        depth = photos.save(request.files['depth'])
+        # raw = photos.save(request.files['raw'])
+        # depth = photos.save(request.files['depth'])
         file_url = photos.url(ir)
+
+        sub = subprocess.Popen(shell, shell=True, cwd=cwd, stdout=subprocess.PIPE)
+        sub.wait()
+        result = sub.stdout.read()
+        print(result)
         return html + '<br><img src=' + file_url + '>'
     return html
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
